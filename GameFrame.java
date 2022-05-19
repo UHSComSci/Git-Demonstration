bb02f465f739591d114766221fb68a23fb671d81
VivienCai <74844967+VivienCai@users.noreply.github.com>
2022-05-19 15:50:25 -0400

    Revert "cube moving"
    This reverts commit 33dae51d835073cf8264491899e827a04df729af.

--- GameFrame.java
+++ GameFrame.java
@@ -1,26 +1,67 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
+import java.awt.geom.*;
 
-public class GameFrame extends JFrame {
-
-    GamePanel panel;
+public class GameFrame implements KeyListener {
+    public static JFrame frame;
 
     public GameFrame() {
-        panel = new GamePanel();
-        this.add(panel);
-        this.setTitle("JavaAppDev Game");
-        this.setResizable(false);
-        this.setBackground(Color.white);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
+        frame = new JFrame();
+        frame.getContentPane().setBackground(Color.WHITE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(600, 600));
+        frame.getContentPane().setLayout(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.addKeyListener(this);
+        frame.pack();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch( keyCode ) { 
+            case KeyEvent.VK_UP:
+                System.out.println("up");
+                break;
+            case KeyEvent.VK_DOWN:
+                System.out.println("down");
+                break;
+            case KeyEvent.VK_LEFT:
+                System.out.println("left");
+                break;
+            case KeyEvent.VK_RIGHT :
+                System.out.println("right");
+                break;
+        }
+        
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch( keyCode ) { 
+            case KeyEvent.VK_UP:
+                System.out.println("up released");
+                break;
+            case KeyEvent.VK_DOWN:
+                System.out.println("down released");
+                break;
+            case KeyEvent.VK_LEFT:
+                System.out.println("left released");
+                break;
+            case KeyEvent.VK_RIGHT :
+                System.out.println("right released");
+                break;
+        }
+        
+    }
     public static void main(String[] args) {
         GameFrame frame = new GameFrame();
     }
-
 }
